8d85c84abf4c277a53e552c3eade7092af7a86de
Akshat Upadhyay <akchatasr@gmail.com>
2023-03-26 00:19:55 +0530

    Added missing comments

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const cardTotalElem = document.getElementById("card-total");
 const loader = document.getElementById("loading");
 
 // cardLimit tells total images to be fetched and cardIncrease tells total images to be shown before end of page is reached
-const cardLimit = 100;
+const cardLimit = 32;
 const cardIncrease = 8;
 const pageCount = Math.ceil(cardLimit / cardIncrease);
 let currentPage = 1;
 
 cardTotalElem.innerHTML = cardLimit;
 
-
+// Constants for the lightbox i.e. the click to expand image functionality
 const lightBoxContainer = document.createElement("div");
 const lightBoxContent = document.createElement("div");
 const lightBoxImg = document.createElement("img");
 const lightBoxPrev = document.createElement("div");
 const lightBoxNext = document.createElement("div");
 
+// setting necessary heirarchy of divs and giving class names
 lightBoxContainer.classList.add("lightbox");
 lightBoxContent.classList.add("lightbox-content");
 lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
@@ -32,22 +33,15 @@ lightBoxContent.appendChild(lightBoxNext);
 lightBoxNext.innerHTML = '>';
 lightBoxPrev.innerHTML = '<';
 
+// adding the lightbox container to the document
+
 document.body.appendChild(lightBoxContainer);
 
-// throttle delays the fetching 
-var throttleTimer;
-const throttle = (callback, time) => {
-    if (throttleTimer) return;
-
-    throttleTimer = true;
-
-    setTimeout(() => {
-        callback();
-        throttleTimer = false;
-        loader.style.visibility = 'hidden';
-        lightBoxNext.style.visibility = 'visible';
-    }, time);
+window.onload = function () {
+    addCards(currentPage);
 };
+
+// Creates new img items inside the card-container div and adds event listener to them to check for clicks on the image
 
 const createCard = (index) => {
     const card = document.createElement("img");
@@ -57,6 +51,8 @@ const createCard = (index) => {
     card.addEventListener("click", currentImage);
     cardContainer.appendChild(card);
 };
+
+// Calls for new images to be added to the grid, Updates images fetched and shows it using the card-actions div
 
 const addCards = (pageIndex) => {
     currentPage = pageIndex;
@@ -68,6 +64,15 @@ const addCards = (pageIndex) => {
         createCard(i);
     }
 };
+
+// Removes the loading animation and the event listener when end of page has been achieved and all images have been loaded
+
+const removeInfiniteScroll = () => {
+    loader.remove();
+    window.removeEventListener("scroll", handleInfiniteScroll);
+};
+
+// Function to execute aftermath of each scroll or when an image is requested(using arrow/next button) which hasn't been fetched yet
 
 const handleInfiniteScroll = (nextFlag = 0) => {
     loader.style.visibility = 'visible';
@@ -83,17 +88,22 @@ const handleInfiniteScroll = (nextFlag = 0) => {
     }, 2000);
 };
 
-const removeInfiniteScroll = () => {
-    loader.remove();
-    window.removeEventListener("scroll", handleInfiniteScroll);
+// throttle delays the fetching 
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+    if (throttleTimer) return;
+
+    throttleTimer = true;
+
+    setTimeout(() => {
+        callback();
+        throttleTimer = false;
+        loader.style.visibility = 'hidden';
+        lightBoxNext.style.visibility = 'visible';
+    }, time);
 };
-
-window.onload = function () {
-    addCards(currentPage);
-};
-
-window.addEventListener("scroll", handleInfiniteScroll);
-
 
 // The below part handles the Lightbox part i.e. click to expand, click to move to next/previous image
 let index = 0;
@@ -127,12 +137,11 @@ function showLightBox(n) {
 
 }
 
-function currentImage() {
+function currentImage() {                                                   //Gets the index of the current image being shown in lightbox
     lightBoxContainer.style.display = "block";
     const imageIndex = parseInt(this.getAttribute("data-index"));
     showLightBox(index = imageIndex);
 }
-
 function slideImage(n) {
     showLightBox(index += n);
 }
@@ -144,10 +153,13 @@ function nextImage() {
 }
 
 
-//arrow buttons functionality
-lightBoxPrev.addEventListener("click", prevImage);
+// event Listeners
+lightBoxPrev.addEventListener("click", prevImage);          //arrow buttons functionality
 lightBoxNext.addEventListener("click", nextImage);
-lightBoxContainer.addEventListener("click", (e) => {
+
+window.addEventListener("scroll", handleInfiniteScroll);    //Event listener for scrolling
+
+lightBoxContainer.addEventListener("click", (e) => {        //Event listener for coming out of lightbox on click
     if (lightBoxContent.contains(e.target)) {
         ;
     } else {
@@ -158,6 +170,7 @@ lightBoxContainer.addEventListener("click", (e) => {
 
 
 // Adding keyboard functionality to move between images
+
 document.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowLeft' || event.code === 'ArrowUp') {
         prevImage();
@@ -170,4 +183,3 @@ document.addEventListener('keydown', (event) => {
         lightBoxImg.removeAttribute("src");
     }
 })
-
